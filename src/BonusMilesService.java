001bf034c0f287bfa3911dcfc21d38d246fc6370
Liza <elizavetamihel9@gmail.com>
2025-01-25 20:13:41 +0300

    Добавляет проект с классом BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class BonusMilesService {
-}
+
+    // Метод для расчета количества бонусных миль
+    public int calculate(int ticketPrice) {
+        // Конверсионный коэффициент: 1 миля за каждые 20 рублей
+        int milesPerUnit = 20;
+        // Рассчитываем количество миль
+        return ticketPrice / milesPerUnit;
+    } }
