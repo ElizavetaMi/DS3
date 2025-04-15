7f06903fd7f722e1ee634873c807276ed31d2c67
Liza <elizavetamihel9@gmail.com>
2025-04-15 22:43:03 +0300

    Удаление лишних файлов из репозитория

--- src/BonusMilesService.java
+++
@@ -1,9 +0,0 @@
-public class BonusMilesService {
-
-    // Метод для расчета количества бонусных миль
-    public int calculate(int ticketPrice) {
-        // Конверсионный коэффициент: 1 миля за каждые 20 рублей
-        int milesPerUnit = 20;
-        // Рассчитываем количество миль
-        return ticketPrice / milesPerUnit;
-    } }
